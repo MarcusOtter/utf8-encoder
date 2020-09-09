16ae6d275c85f32374410f37ce64a5e25a30e639
LeMorrow <marcus.otterstrom@gmail.com>
2020-09-09 23:02:28 +0200

    Add working numeral system conversions

--- src/js/main.js
+++ src/js/main.js
@@ -14,8 +14,8 @@ const offsetNumberInput = document.getElementById("js-offset-input-number");
 const buttonNotFilledClassName = "button-outline";
 
 let isEncoding = true;
-let currentNumeralSystem = "BIN";
-let currentOffset = 0;
+let currentBase = 2;
+let currentOffsetDecimal = 0;
 
 function setIsEncoding(shouldEncode) {
     if (isEncoding === shouldEncode) { return; }
@@ -29,29 +29,125 @@ function setIsEncoding(shouldEncode) {
     isEncoding = shouldEncode;
 }
 
-function setNumeralSystem(newSystem) {
-    binaryButton.classList.toggle(buttonNotFilledClassName, newSystem !== "BIN");
-    hexadecimalButton.classList.toggle(buttonNotFilledClassName, newSystem !== "HEX");
-    decimalButton.classList.toggle(buttonNotFilledClassName, newSystem !== "DEC");
+function setBase(newBase) {
+    binaryButton     .classList.toggle(buttonNotFilledClassName, newBase !== 2);
+    hexadecimalButton.classList.toggle(buttonNotFilledClassName, newBase !== 16);
+    decimalButton    .classList.toggle(buttonNotFilledClassName, newBase !== 10);
     
-    currentNumeralSystem = newSystem;
+    currentBase = newBase;
+    
+    updateOffsetValue(currentOffsetDecimal, 10, currentBase);
 }
 
-function updateOffsetValue(newOffset) {
-    offsetRangeInput.value = newOffset;
-    offsetNumberInput.value = newOffset;
+// 8-bit signed integer
+function decimalToStringWithNewBase(decimalValue, newBase) {
+    // If negative number, start counting backwards from 255 (2-complement)
+    if (newBase !== 10 && decimalValue < 0) {
+        decimalValue =  0xFF + decimalValue + 1;
+    }
+
+    output = parseInt(decimalValue, 10).toString(newBase);
+
+    if (newBase === 2) {
+        output = formatBinary(output);
+    }
+    else if (newBase === 16) {
+        output = formatHexadecimal(output);
+    }
+    else if (newBase === 10) {
+        output = formatDecimal(output);
+    }
+
+    return output;
+}
+
+function binaryToDecimal(binaryValue) {
+    binaryValue = binaryValue.toString().replace(/\s+/g, '');
+
+    // Why doesn't parseInt() understand signed integers :(
+    const isNegative = binaryValue[0] === "1";
+
+    return isNegative
+        ? -(0xFF + 1) + parseInt(binaryValue, 2)
+        : parseInt(binaryValue, 2);
+}
+
+function hexadecimalToDecimal(hexadecimalValue) {
+    const negatives = ["8", "9", "A", "B", "C", "D", "E", "F"];
+    const isNegative = negatives.includes(hexadecimalValue[2]);
+
+    return isNegative
+        ? -(0xFF + 1) + parseInt(hexadecimalValue, 16)
+        : parseInt(hexadecimalValue, 16);
+}
+
+function formatBinary(binaryString) {
+    // Append zeroes if MSB not in full group of 4
+    let lengthMod4 = binaryString.length % 4;
+    if (lengthMod4 !== 0) {
+        binaryString = "0".repeat(4 - lengthMod4) + binaryString;
+    }
+
+    if (binaryString.length === 4) {
+        binaryString = "0000" + binaryString;
+    }
     
-    currentOffset = newOffset;
+    // Add spacing between each group of 4 bits
+    let output = "";
+    for (let i = 0; i < binaryString.length; i += 4) {
+        output += binaryString.substr(i, 4) + " ";
+    }
+    
+    return output.trim();
+}
+
+function formatHexadecimal(hexadecimalString) {
+    if (hexadecimalString.length === 1) {
+        hexadecimalString = "0" + hexadecimalString;
+    }
+
+    return "0x" + hexadecimalString.toUpperCase();
+}
+
+function formatDecimal(decimalString) {
+    return parseInt(decimalString).toLocaleString();
+}
+
+function updateOffsetValue(numberString, numberStringBase, newBase) {
+    // Remove whitespace
+    numberString = numberString.toString().replace(/\s+/g, '');
+
+    let number = 0;
+    if (numberStringBase === 2) {
+        number = binaryToDecimal(numberString);
+    }
+    else if (numberStringBase === 16) {
+        number = hexadecimalToDecimal(numberString);
+    }
+    else if (numberStringBase === 10) {
+        number = parseInt(numberString, 10);
+    }
+
+    if (number > 127)  { number =  127; }
+    if (number < -128) { number = -128; }
+
+    let numberStringNewBase = decimalToStringWithNewBase(number, newBase);
+
+    offsetRangeInput .value = number;
+    offsetNumberInput.value = numberStringNewBase;
+    
+    currentOffsetDecimal = number;
 }
 
 encodeButton.onclick = () => setIsEncoding(true);
 decodeButton.onclick = () => setIsEncoding(false);
 
-binaryButton     .onclick = () => setNumeralSystem("BIN");
-hexadecimalButton.onclick = () => setNumeralSystem("HEX");
-decimalButton    .onclick = () => setNumeralSystem("DEC");
+binaryButton     .onclick = () => setBase(2);
+hexadecimalButton.onclick = () => setBase(16);
+decimalButton    .onclick = () => setBase(10);
 
-offsetRangeInput .oninput = () => updateOffsetValue(offsetRangeInput.value);
-offsetNumberInput.oninput = () => updateOffsetValue(offsetNumberInput.value);
+offsetRangeInput .oninput  = () => updateOffsetValue(offsetRangeInput.value, 10, currentBase);
+offsetNumberInput.onchange = () => updateOffsetValue(offsetNumberInput.value, currentBase, currentBase);
 
-updateOffsetValue(0);
+setBase(2);
+updateOffsetValue(0, 10, currentBase);
